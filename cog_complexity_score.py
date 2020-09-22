a0e5bafd14f08d1f431b957587aa0ab1e4d294b4
Guide Analytics <guideanalytics2020@gmail.com>
2020-09-22 14:35:04 +0000

    Update cog_complexity_score.py

--- cog_complexity_score.py
+++ cog_complexity_score.py
@@ -145,9 +145,15 @@ file_change_df = calculated_df.withColumn('ceo_year_name', filenameChange(col('c
 
 file_change_df = file_change_df.filter(col('token_count') >= 250)
 
+## IMPORTANT!!!!!!!
+# If the program crashes here, you may need to comment this section out from line 155-160. 
+# Replace in line 157 
+# From: file_name_change_df 
+# To: file_change_df
+# file_change_df = file_change_df ...
 
 split_col = split(col('ceo_year_name'), '_')
-file_name_change_df = file_change_df.withColumn('company', split_col.getItem(0)).cache()
+file_name_change_df = file_change_df.withColumn('', split_col.getItem(0)).cache() 
 file_name_change_df = file_name_change_df.withColumn('name', split_col.getItem(1))
 file_name_change_df = file_name_change_df.withColumn('year', split_col.getItem(2))
 
